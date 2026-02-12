258d50da9ab76f9cbdd059316b240965107eaf99
Kumari Anshika <129029842+Anshika070@users.noreply.github.com>
2026-02-12 19:09:46 +0530

    Function

--- .vscode/functions.cpp
+++ .vscode/functions.cpp
@@ -5,5 +5,10 @@ int sum(int a,int b){
 }
 int main(){
      cout<<sum(5,6)<<endl;
+     int num1;
+     int num2;
+     cin>>num1;
+     cin>>num2;
+     cout<<sum(num1,num2)<<endl;
     return 0;
 }
